fd8d3d9497b7107b93cc3ba941bb8737ecdcfd8e
MarinaOn <firecat752@gmail.com>
2020-03-30 00:06:49 +0300

    Lecture 6 Home Task - String

--- src/main/java/com/hillel/lecture_6/UpperLowerCaseChecker.java
+++ src/main/java/com/hillel/lecture_6/UpperLowerCaseChecker.java
@@ -34,10 +34,26 @@ public class UpperLowerCaseChecker {
         sentenceChar = sentence.toCharArray();
         sentenceChar[0] = Character.toUpperCase(sentenceChar[0]);
         for (int i = 1; i < sentenceChar.length; i++) {
-            if ((sentenceChar[i] == '.' || sentenceChar[i] == '!' || sentenceChar[i] == '?') && (i+2) <= sentenceChar.length) {
-                sentenceChar[i+2] = Character.toUpperCase(sentenceChar[i+2]);
+            if ((sentenceChar[i] == '.' || sentenceChar[i] == '!' || sentenceChar[i] == '?') && (i + 2) <= sentenceChar.length) {
+                sentenceChar[i + 2] = Character.toUpperCase(sentenceChar[i + 2]);
             }
             result = new String(sentenceChar);
+        }
+
+        return result;
+
+    }
+
+    @Step
+    public String eachLetterToUpperCase(String sentence) {
+        char[] sentenceChar;
+        String result = "";
+        String[] tempStringArray = sentence.split(" ");
+        for (int i = 0; i < tempStringArray.length; i++) {
+            sentenceChar = tempStringArray[i].toCharArray();
+            sentenceChar[0] = Character.toUpperCase(sentenceChar[0]);
+            result = result + new String(sentenceChar);
+            if (i != tempStringArray.length - 1) result = result + " ";
         }
 
         return result;
